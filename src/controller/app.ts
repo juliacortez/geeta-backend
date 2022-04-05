de6ed54831b616b290e9f691d107ec24f3b9eabd
Julia Cortez Simao <juliacortez984@gmail.com>
2022-04-05 19:27:08 -0300

    deploy20

--- src/controller/app.ts
+++ src/controller/app.ts
@@ -9,5 +9,5 @@ app.use(express.json())
 app.use(cors())
 
 app.listen(process.env.PORT || 3003, () => {
-    console.log('Server Ready')
+    console.log(`Servidor pronto na porta ${process.env.PORT}.`)
 })
